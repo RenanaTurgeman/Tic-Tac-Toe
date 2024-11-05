e2840fec55af6404bd2be26a850dddaff15f696f
Renana Turgeman <renanaturgeman058@gmail.com>
2024-11-05 09:17:41 +0200

    אןאךק

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -83,12 +83,15 @@ export default function Game() {
   });
 
   return (
-    <div className="game">
-      <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
-      </div>
-      <div className="game-info">
-        <ol>{moves}</ol>
+    <div className="container">
+      <h1 className="title">Tic Tac Toe</h1>
+      <div className="game">
+        <div className="game-board">
+          <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        </div>
+        <div className="game-info">
+          <ol>{moves}</ol>
+        </div>
       </div>
     </div>
   );
