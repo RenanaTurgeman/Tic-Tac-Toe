70bfc5c384836bf95c6f98a35359b812a5e563d1
Renana Turgeman <renanaturgeman058@gmail.com>
2024-11-06 10:31:24 +0200

    tie message

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -31,6 +31,8 @@ function Board({ xIsNext, squares, onPlay }) {
   let status;
   if (winner) {
     status = 'Winner: ' + winner;
+  }else if (!squares.includes(null)) {
+    status = "It's a tie!";
   } else {
     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
